df714a279d9f472dcee8a4ddb27459405b3a5730
Ester Castellano Rios <escastel@c3r6s6.42malaga.com>
2024-06-28 15:29:45 +0200

    cambios para errores de compilacion

--- ft_count_unsignb.c
+++ ft_count_unsignb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_count_unsignb.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: escastel <escastel@42.fr>                  +#+  +:+       +#+        */
+/*   By: escastel <escastel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 13:53:01 by escastel          #+#    #+#             */
-/*   Updated: 2023/05/25 14:10:08 by escastel         ###   ########.fr       */
+/*   Updated: 2024/06/28 15:28:50 by escastel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_count_unsignb(unsigned int nb)
 	int	count;
 
 	count = 0;
-	if (nb <= -1)
-		count += 1;
 	while (nb >= 10)
 	{
 		nb /= 10;
--- ft_putunsignbr.c
+++ ft_putunsignbr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putunsignbr.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: escastel <escastel@42.fr>                  +#+  +:+       +#+        */
+/*   By: escastel <escastel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 11:06:36 by escastel          #+#    #+#             */
-/*   Updated: 2023/05/25 14:10:23 by escastel         ###   ########.fr       */
+/*   Updated: 2024/06/28 15:21:10 by escastel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_putunsignbr(unsigned int nb)
 	int	count;
 
 	count = ft_count_unsignb(nb);
-	if (nb >= 0 && nb <= 9)
+	if (nb <= 9)
 	{
 		nb += '0';
 		write(1, &nb, 1);
